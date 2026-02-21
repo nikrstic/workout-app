03c5b3659ec56a9fdd4f128be281c4ff0359453a
nikola <nikrstic@outlook.com>
2026-02-22 00:15:50 +0100

    napravljena bottom navigacija

--- app/src/main/java/com/example/myapplication/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/auth/AuthViewModel.kt
@@ -1,14 +1,22 @@
 package com.example.myapplication.ui.auth
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myapplication.data.auth.local.TokenStorage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(
-    tokenStorage: TokenStorage
+    private val tokenStorage: TokenStorage
 ): ViewModel() {
 
     val token = tokenStorage.token
+    fun logout(){
+        viewModelScope.launch {
+            tokenStorage.clearToken()
+        }
+    }
 }
