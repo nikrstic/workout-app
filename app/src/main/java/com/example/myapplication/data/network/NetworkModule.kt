3fe19baa5036a077d777fc0a67ac276b10f86e30
nikola <nikrstic@outlook.com>
2026-02-22 21:09:52 +0100

    poziva se udaljen server, resen problem sa bottom navigacijom i dodaj je disabled dok se ne popune polja

--- app/src/main/java/com/example/myapplication/data/network/NetworkModule.kt
+++ app/src/main/java/com/example/myapplication/data/network/NetworkModule.kt
@@ -15,8 +15,8 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
-
+    //private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "http://nikrstic.klzx.xyz:8080/"
     @Provides
     @Singleton
     fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
